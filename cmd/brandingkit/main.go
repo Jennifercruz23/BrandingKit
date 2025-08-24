4eb3450782c16c23b52b3525e002fbdbf8e9a65f
Jennifer <142938327+Jennifercruz23@users.noreply.github.com>
2025-08-24 07:33:37 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/brandingkit/main.go
@@ -0,0 +1,23 @@
+// cmd/brandingkit/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "brandingkit/internal/brandingkit"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := brandingkit.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
